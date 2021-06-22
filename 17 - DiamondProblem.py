cb6a0948bfa8906c39717477dceb3e694f74f0d7
idzharulhuda13 <idzharul.huda@gmail.com>
2021-06-22 19:26:00 +0700

    DiamondProblem

---
+++ 17 - DiamondProblem.py
@@ -0,0 +1,25 @@
+class A:
+
+    def show(self):
+        print("ini adalah show a")
+
+
+class B(A):
+
+    def show(self):
+        print("ini adalah show b")
+
+
+class C(A):
+
+    def show(self):
+        print("ini adalah show c")
+
+
+class D(B, C):
+    pass
+
+
+d = D()
+# d.show()
+help(d)
